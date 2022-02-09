ae789626aa4d29acef20d868fe0abae94c832f3a
Nahuel77 <70348604+Nahuel77@users.noreply.github.com>
2022-02-09 20:10:12 -0300

    Add files via upload

---
+++ gallery-demo/main.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+
+	var thispic;
+	var actual;
+
+	$('.pic').click(show);
+	$('#prev').click(prevPic);
+	$('#next').click(nextPic);
+	$('#close').click(close);
+	$(document).keydown(keylistener);
+
+	function keylistener(e){
+		var keyID = e.which;
+		switch(keyID){
+			case 27:
+				close();
+				break;
+			case 37:
+				prevPic();
+				break;
+			case 39:
+				nextPic();
+				break;
+		}
+	}
+
+	function show(){
+		$('#showcase').css('visibility', 'visible');
+		thispic = $(this);
+		var pic = thispic.attr('src');
+		$('#thispic').attr('src', pic);
+		actualPic();
+	}
+
+	function actualPic(){
+		actual = $('#thispic').attr('src');
+		thispic = $('.pic[src="'+actual+'"]');
+	}
+
+	function prevPic(){
+		var pic = thispic.prev().attr('src');
+		$('#thispic').attr('src', pic);
+		actualPic();
+	}
+
+	function nextPic(){
+		var pic = thispic.next().attr('src');
+		$('#thispic').attr('src', pic);
+		actualPic();
+	}
+
+	function close(){
+		$('#showcase').css('visibility', 'hidden');
+	}
+
+});
